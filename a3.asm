0bbf4a3deee45ef1227beb84e224009455978c35
Robin <rbechtel.ostmann@googlemail.com>
2016-05-11 21:31:17 -0400

    fixed a3, added printf

--- a3.asm
+++ a3.asm
@@ -2,32 +2,34 @@ SECTION .data
 x DD 4
 ergebnis DD 0
 koeffizient DD 33,2,7
+msg DB "Ergebnis: %d", 10, 0
 
 SECTION .text
-global main
+global _start
 extern printf
 
-main:
+_start:
 push ebp
 mov ebp, esp
 
-        mov ebx, dword [koeffizient]	; ebx = &koeffizient
-	mov eax, x			; eax = x
+        mov ebx, koeffizient		; ebx = &koeffizient
+	mov eax, [x]			; eax = x
 	imul eax 			; x²
-	imul eax, dword [ebx + 0 * 2] 	; eax *= ebx + 0 * 2 (ebx-base, 0-index, 2-scale)
+	imul eax, dword [ebx + 0 * 4] 	; eax *= ebx + 0 * 4 (ebx-base, 0-index, 4-scale)
 	push eax 			; save eax=ax² on stack
-	mov eax, x			; eax = x
-	imul eax, dword [ebx + 1 * 2]	; eax *= ebx + 0 * 2 
+	mov eax, [x]			; eax = x
+	imul eax, dword [ebx + 1 * 4]	; eax *= ebx + 0 * 4 
 	push  eax			; save eax = bx
-	mov eax, dword [ebx + 2 * 2]	; eax = c
+	mov eax, dword [ebx + 2 * 4]	; eax = c
 	add eax, dword [esp]		; eax += current stack value(= bx)
-	dec esp				; decrement stackpointer
+	add esp, 4			; stackpointer + 4 
 	add eax, dword [esp] 		; eax += current stack value(= ax²)
 	mov dword [ergebnis], eax	; save eax in ergebnis(= c+bx+ax²)
 
-	push eax
+	push dword [ergebnis]
+	push msg
 	call printf
-	add esp, 4
+	add esp, 8
 
 mov esp, ebp
 pop ebp
@@ -35,4 +37,3 @@ pop ebp
 mov ebx, 0
 mov eax, 1
 int 0x80
-
--- makefile
+++ makefile
@@ -2,7 +2,15 @@ asm = nasm
 ld = ld
 
 asm_flags = -f elf -F dwarf -g
-ld_flags = -lc
+ld_flags = -lc -I/lib/ld-linux.so.2 
+
+all: a1 a2 a3 hi beer
+	make a1
+	make a2
+	make a3
+	make hi
+	make beer
+	make clean
 
 a3: a3.o
 	${ld} ${ld_flags} -o a3 a3.o
@@ -29,6 +37,9 @@ hi.o: hi.asm
 	${asm} ${asm_flags} hi.asm
 
 clean:
+	rm -f *.o
+
+cleaner:
 	rm -f hi hi.o a1 a1.o a2 a2.o a3 a3.o
 
 beer:
